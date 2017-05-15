8adf5c3f8589b61b3d5a869694e3b02fbed3b97a
hubig <hubig1@stolaf.edu>
2017-05-14 21:02:16 -0500

    Add files via upload

--- poem gen gui.py
+++ poem gen gui.py
@@ -10,26 +10,32 @@ one button that says "generate"""
 
 class user_gui:
         def __init__(self):
-                self.window = tk.Tk()
-                self.create_widgets()
+                self.root= tk.Tk() #root is window
+                self.create_widgets() #inits the gui with the create_widgets function
                 
         def create_widgets(self):
-                s = ttk.Style()
-                s.configure('.', font=('Helvetica', 12))
-                label = ttk.Label(self.window, text="Hello World")
-                self.label_text = label.grid(row=0,column=0)
+                """Textbox"""
+                s = ttk.Style() #using ttk style!
+                s.configure('.', font=('Helvetica', 12), sticky=tk.N+tk.E+tk.S+tk.W, padx=10) #'.' is a root style; this line is used to configure an overall style
+                label = ttk.Label(self.root, text="""ABOUT
+This is a random poem generator created by Charlie Carlson, Iain Irwin, and Nic Hubig for the CSCI121 final project.""")
+                self.label_text = label.grid(row=1, column=1, columnspan=2,)
+
+                """Slider scale"""
+                #This decides how many lines of a poem to generate
+                scale = ttk.Scale(self.root, from_=1, to=100)
+                scale.grid(row=2, column=1, columnspan=2, sticky=tk.N+tk.E+tk.S+tk.W, padx=10)
 
                 """Generate button"""
-                generate = ttk.Button(self.window, text="Generate", )
-                generate.grid(row=1,column=0)
+                generate = ttk.Button(self.root, text="Generate poetry")
+                generate.grid(row=3, column=1)
                 #generate['command'] = self.function()
 
                 """Quit button"""
-                quit_button = ttk.Button(self.window, text="Quit")
-                quit_button.grid(row=2,column=0)
-                quit_button['command'] = self.window.destroy
+                quit_button = ttk.Button(self.root, text="Quit")
+                quit_button.grid(row=3, column=2)
+                quit_button['command'] = self.root.destroy
 
 
 program = user_gui()
-program.window.mainloop()
-
+program.root.mainloop()
